705fe69dd63a507d80b1c8a33414fdddb1bdbbee
nurrachmat <rachmat.nur@gmail.com>
2025-05-14 15:10:32 +0700

    store fakultas

--- app/Http/Controllers/FakultasController.php
+++ app/Http/Controllers/FakultasController.php
@@ -29,7 +29,19 @@ class FakultasController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validasi input
+        $input = $request->validate([
+            'nama' => 'required|unique:fakultas',
+            'singkatan' => 'required|max:5',
+            'dekan' => 'required',
+            'wakil_dekan' => 'required',
+        ]);
+
+        // simpan data ke tabel fakultas
+        Fakultas::create($input);
+
+        // redirect ke route fakultas.index
+        return redirect()->route('fakultas.index')->with('success', 'Fakultas berhasil ditambahkan.');
     }
 
     /**
